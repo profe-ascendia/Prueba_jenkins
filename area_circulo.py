3d2be9799de53f3168d49c85c5facfeea4977f9c
Teo <ascendia.teosanchez@gmail.com>
2020-12-16 14:18:03 +0100

    nada

--- area_circulo.py
+++ area_circulo.py
@@ -1,19 +1,14 @@
-    """  
-    if type(radio) == complex:
-        raise ValueError('El radio no puede ser complejo')
+import math
 
-    if radio is None:
-        return None
-    if type(radio ) == str:
-        raise ValueError('El radio debe ser un número')
-    if radio < 0:
-        raise ValueError('El radio no puede ser negativo')    
-    """
+
+def area(radio): 
     #Refactorizado
+    
     if not type(radio) in [int, float] :
         raise TypeError('El radio debe ser un número entero o decimal mayor que cero')
 
     if  radio <0:
         raise ValueError('El radio no puede ser negativo')
-
+    
     return math.pi * radio * radio
+
